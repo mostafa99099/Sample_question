7836d35d1656c07a92c07a6da3786b9f9670f0fa
mostafa <115219911+mostafa99099@users.noreply.github.com>
2023-06-08 21:22:52 -0700

    Update list.py

--- list/list.py
+++ list/list.py
@@ -1,3 +1,5 @@
 my_list = ["mostafa", 4.6, 12, True]
 for indx,item in enumerate(my_list):
     print(f"indx => {indx} . item => {item}")
+    if indx == 2:
+        print("hoooooraaaaaaaaaaaaaaaa")
